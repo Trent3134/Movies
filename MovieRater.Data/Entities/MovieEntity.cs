4722d6f656ddbfa3f4ccc0fdb3ad3fcd65d4b9ca
trent3134 <trentconway@yahoo.com>
2022-06-16 18:36:58 -0400

    Genre

--- MovieRater.Data/Entities/MovieEntity.cs
+++ MovieRater.Data/Entities/MovieEntity.cs
@@ -10,6 +10,5 @@ using System.Threading.Tasks;
         public string Name { get; set; }
         public string Review { get; set; }
         public string  Cast { get; set; }
-        public int MyProperty { get; set; }
         public GenreType GenreType { get; set; }
     }
