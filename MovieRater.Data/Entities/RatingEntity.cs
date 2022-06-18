3be6c9382f23b980bdf7e113c58aaf50fbe5f72a
Tracy Redman <redmanatracy@gmail.com>
2022-06-18 12:25:58 -0400

    UpdatedRatingEntityTable

--- MovieRater.Data/Entities/RatingEntity.cs
+++ MovieRater.Data/Entities/RatingEntity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,12 +8,15 @@ using System.Threading.Tasks;
 
     public class RatingEntity
     {
+        public int Id { get; set; }
+        [Range(1,5)]
+        public double Rating { get; set; }
         // forgign key points to a movie or show
         [ForeignKey(nameof(MovieEntity))]
-        public int MovieEntityId { get; set; }
+        public int? MovieEntityId { get; set; }
         public MovieEntity MovieEntity { get; set; }
-        
+
         [ForeignKey(nameof(ShowEntity))]
-        public int ShowEntityId { get; set; }
+        public int? ShowEntityId { get; set; }
         public ShowEntity ShowEntity { get; set; }
     }
